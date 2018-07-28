74e4163e1776739acc45a58731c6e27e08f3d7e6
Haider Malik <haidermalik504@gmail.com>
2018-07-28 17:56:08 +0500

    add root

--- src/index.js
+++ src/index.js
@@ -4,8 +4,27 @@ import { render } from 'react-dom';
 const node = document.getElementById('root');
 
 const root = React.createElement(
-    'h1',
+    'div',
     {},
-    'Hello React');
+    //array of props or children
+    'Hello React',
+    'Hello world',
+    'I am learning React',
+    React.createElement(
+        'a',
+        { href: '/' },
+        'Learn React'),
+    'Hello world',
+
+    React.createElement(
+        'div',
+        {},
+        React.createElement(
+            'p',
+            {},
+            'I am paragraph inside div')
+    )
+);
+console.log(root);
 
 render(root, node);
