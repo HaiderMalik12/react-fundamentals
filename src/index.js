faff53957aec7a972bc5508b4919b92b40224ca5
Haider Malik <haidermalik504@gmail.com>
2018-07-30 12:26:21 +0500

    create nested component

--- src/index.js
+++ src/index.js
@@ -13,7 +13,9 @@ class Project extends Component {
             React.createElement(
                 'p',
                 {},
-                this.props.description)
+                this.props.description,
+                this.props.children,
+            )
         )
     }
 }
@@ -23,11 +25,34 @@ Project.propTypes = {
     name: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired
 }
+{/* <Project>
+ <Task>
+  <Category/>
+ </Task>
+ //props.children
+</Project>     */}
+class Task extends Component {
+    render() {
+        return React.createElement(
+            'div',
+            { className: 'taskName' },
+            this.props.name
+
+            //this.props.children
+        )
+    }
+}
+//create another component Category
 
 const App = React.createElement(Project, {
     name: 'Learn React by building 10 Projects',
     id: 1,
     description: 'You will learn React Fundamentals and Real world apps'
-});
+},
+    React.createElement(Task, {
+        id: 1,
+        name: 'Create Fundamenatls modoule'
+    })
+);
 
 render(App, node);
