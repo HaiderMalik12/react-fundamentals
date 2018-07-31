3eebb848c0cf3139698d22dbaae2342507932463
Haider Malik <haidermalik504@gmail.com>
2018-07-31 16:28:32 +0500

    deep shallow merge

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import PropTypes from 'prop-types';
+import deepMerge from 'deepmerge';
 
 const node = document.getElementById('root');
 
@@ -10,7 +11,11 @@ class App extends Component {
         this.state = {
             todo: {
                 id: 1,
-                title: 'Learn React'
+                title: 'Learn React',
+                category: {
+                    id: 1,
+                    name: 'Programming'
+                }
             }
         }
     }
@@ -21,11 +26,33 @@ class App extends Component {
             }
         }))
     }
+    changeCategoryHandler = () => {
+        // this.setState(() => ({
+        //     todo: {
+        //         category: {
+        //             name: 'Personal'
+        //         }
+        //     }
+        // }))
+        //1. ES6 way
+        const updatedCategory = { category: { name: 'Personal' } };
+        // this.setState({
+        //     todo: { ...this.state.todo, ...updatedCategory }
+        // })
+        // this.setState({
+        //     todo: Object.assign({}, this.state.todo, updatedCategory)
+        // })
+
+        const updatedTodo = deepMerge(this.state.todo, updatedCategory);
+        this.setState({ todo: updatedTodo });
+    }
     render() {
         return (
             <div>
                 <h3> TODO Title:  {this.state.todo.title}</h3>
+                <p>Category: {this.state.todo.category.name} </p>
                 <button onClick={this.changeTitleHandler}> Change title</button>
+                <button onClick={this.changeCategoryHandler}> Change Category</button>
             </div>
         )
     }
