a740084575eb9bca88a82dde4488091306ca4633
Haider Malik <haidermalik504@gmail.com>
2018-07-31 18:53:41 +0500

    create stateless functional component

--- src/index.js
+++ src/index.js
@@ -1,40 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 import PropTypes from 'prop-types';
 
 const node = document.getElementById('root');
 
-class Counter extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            count: 1
-        }
-    }
-    static defaultProps = {
-        incrementBy: 1
-    }
-    incrementHanlder = () => {
-        // this.props.incrementBy = 12
-        this.setState((prevState, props) => {
-            //increment the count with previous value
-            console.log(prevState, props);
-            return {
-                count: prevState.count + props.incrementBy
-            }
-
-        })
-    }
-    render() {
-        return (
-            <div>
-                <h3> {this.state.count}</h3>
-                <button onClick={this.incrementHanlder}>+</button>
-            </div>
-        )
-    }
+const Greet = (props) => {
+    return (
+        <div> Hello from {props.name}</div>
+    )
 }
-Counter.propTypes = {
-    incrementBy: PropTypes.number.isRequired
+Greet.propTypes = {
+    name: PropTypes.string.isRequired
 }
-render(<Counter incrementBy={1} />, node);
+Greet.defaultProps = {
+    name: 'John'
+}
+render(<Greet />, node);
