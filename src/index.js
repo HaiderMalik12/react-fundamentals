e3eb01ebf3161457f824e6d5ee9fd9ef2c4fcffc
Haider Malik <haidermalik504@gmail.com>
2018-08-01 10:30:24 +0500

    component communication

--- src/index.js
+++ src/index.js
@@ -1,18 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { render } from 'react-dom';
 import PropTypes from 'prop-types';
 
 const node = document.getElementById('root');
 
-const Greet = (props) => {
+const data = [
+    { id: '1', name: 'Module # 1' },
+    { id: '2', name: 'Module # 2' },
+    { id: '3', name: 'Module # 3' },
+]
+//stateful
+class Project extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: '1',
+            name: 'Learn React',
+            tasks: this.props.tasks
+        }
+    }
+    render() {
+        return (
+            <div>
+                <h3> Project Name : {this.state.name} </h3>
+                <h3>Task Card </h3>
+                <TaskCard tasks={this.state.tasks} />
+                <h3> Task Table</h3>
+                <TaskTable tasks={this.state.tasks} />
+            </div>
+        )
+    }
+}
+//child component
+const TaskCard = props => {
     return (
-        <div> Hello from {props.name}</div>
+        <div>
+            {props.tasks.map(task => (
+                <li>{task.name} </li>
+            ))}
+        </div>
     )
 }
-Greet.propTypes = {
-    name: PropTypes.string.isRequired
+const TaskTable = props => {
+    return (
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.tasks.map(task => (
+                        <tr>
+                            <td> {task.id}</td>
+                            <td> {task.name}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
 }
-Greet.defaultProps = {
-    name: 'John'
-}
-render(<Greet />, node);
+
+render(<Project tasks={data} />, node);
