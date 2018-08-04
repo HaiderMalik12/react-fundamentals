0876aad0c1bad886966fbc9d2a3607ba8cfdf94f
Haider Malik <haidermalik504@gmail.com>
2018-08-04 12:05:17 +0500

    component mounting lifecyle methods in action

--- src/index.js
+++ src/index.js
@@ -1,67 +1,80 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 const node = document.getElementById('root');
 
-const data = [
-    { id: '1', name: 'Module # 1' },
-    { id: '2', name: 'Module # 2' },
-    { id: '3', name: 'Module # 3' },
-]
-//stateful
-class Project extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            id: '1',
-            name: 'Learn React',
-            tasks: this.props.tasks
-        }
-    }
-    render() {
-        return (
-            <div>
-                <h3> Project Name : {this.state.name} </h3>
-                <h3>Task Card </h3>
-                <TaskCard tasks={this.state.tasks} />
-                <h3> Task Table</h3>
-                <TaskTable tasks={this.state.tasks} />
-            </div>
-        )
-    }
-}
-//child component
-const TaskCard = props => {
+class ChildComponent extends Component {
+  constructor(props) {
+    super(props);
+    console.log('ChildComponent: constructor');
+  }
+  componentWillMount() {
+    console.log('ChildComponent: componentWillMount');
+  }
+  componentDidMount() {
+    console.log('ChildComponent: componentDidMount');
+  }
+  render() {
+    console.log('ChildComponent: render');
     return (
-        <div>
-            {props.tasks.map(task => (
-                <li>{task.name} </li>
-            ))}
-        </div>
-    )
-}
-const TaskTable = props => {
-    return (
-        <div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Id</th>
-                        <th>Name</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {props.tasks.map(task => (
-                        <tr>
-                            <td> {task.id}</td>
-                            <td> {task.name}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    )
+      <div>
+        <p> Message : {this.props.msg} </p>
+      </div>
+    );
+  }
 }
 
-render(<Project tasks={data} />, node);
+class ChildComponent2 extends Component {
+  constructor(props) {
+    super(props);
+    //   console.log('ChildComponent: constructor');
+  }
+  componentDidMount() {
+    console.log('ChildComponent2: componentDidMount');
+  }
+  render() {
+    //   console.log('ChildComponent: render');
+    return (
+      <div>
+        <p> Message : {this.props.msg} </p>
+      </div>
+    );
+  }
+}
+class ParentComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      msg: ''
+    };
+    console.log('ParentComponent: constructor');
+  }
+  componentWillMount() {
+    console.log('ParentComponent: componentWillMount');
+  }
+  componentDidMount() {
+    console.log('ParentComponent: componentDidMount');
+  }
+  onChangeHandler = event => {
+    this.setState({ msg: event.target.value });
+  };
+  render() {
+    console.log('ParentComponent: render');
+    return (
+      <div>
+        <h2>LifeCycle methods</h2>
+        <p> Enter your message : </p>
+        <input
+          onChange={this.onChangeHandler}
+          type="text"
+          value={this.state.msg}
+        />
+        <ChildComponent msg={this.state.msg} />
+        <ChildComponent2 msg={this.state.msg} />
+      </div>
+    );
+  }
+}
+
+render(<ParentComponent />, node);
